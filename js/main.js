328952be1481cb35b69ffa24fb3100edcd291a2c
catanielena <catani.elena1@gmail.com>
2021-09-01 17:51:44 +0200

    Add: grid__square--proportion

--- js/main.js
+++ js/main.js
@@ -3,11 +3,30 @@
 // * stabiliti la classe css delle celle "cellClass".
 // * Al click l'aspetto delle celle varia in base alla clasae "addClassToCell"
 // *
+// function createGrid(cellNumb, getFieldId, cellClass, addClassToCell) {
+//     for (let i = 1; i<= cellNumb; i++) {
+//         getFieldId.innerHTML += `<div class=${cellClass}>${i}</div>`;
+//     }   
+    
+//     getFieldId.addEventListener("click",
+//     function(event) {
+//         console.log(event.target.innerHTML);
+//         event.target.classList.toggle(addClassToCell);
+//     }
+// ); 
+// }
+
+
 function createGrid(cellNumb, getFieldId, cellClass, addClassToCell) {
-    for (let i = 1; i<= cellNumb; i++) {
-        getFieldId.innerHTML += `<div class=${cellClass}>${i}</div>`;
+    for (let i = 0; i< cellNumb; i++) {
+        for (let j = 1; j<= cellNumb; j++) {        
+            document.getElementsByClassName("row")[i].innerHTML += `<div class=${cellClass}>${i}</div>`;
+        }  
     }   
     
+    for (let k = cellNumb + 1; k <= 10; k++) {
+        document.getElementsByClassName("row")[k].classList.add("display--none");
+    }
     getFieldId.addEventListener("click",
     function(event) {
         console.log(event.target.innerHTML);
@@ -15,7 +34,6 @@ function createGrid(cellNumb, getFieldId, cellClass, addClassToCell) {
     }
 ); 
 }
-
 // Celle per riga
 var userNumb = parseInt(prompt(`Definisci il numero di celle di cui sarà composto il campo da gioco 
 (minimo 1, massimo 100)`));
